f4a347080ba0ceff7ae34abb8eb897cf14e11032
PadLifter <lentodrone@gmail.com>
2023-02-09 10:53:41 +0200

    Fixed project bones

---
+++ MUX/MUX.srcs/sources_1/new/tb_mux2to1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.02.2023 10:52:45
+// Design Name: 
+// Module Name: tb_mux2to1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_mux2to1(
+
+    );
+endmodule
