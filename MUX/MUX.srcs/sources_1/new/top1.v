fadefbe89b70d508df97ea5bff441709b5cb61aa
PadLifter <lentodrone@gmail.com>
2023-02-09 11:19:43 +0200

    Tested schematic

--- MUX/MUX.srcs/sources_1/new/top1.v
+++ MUX/MUX.srcs/sources_1/new/top1.v
@@ -1,26 +1,23 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.02.2023 10:51:51
-// Design Name: 
-// Module Name: top1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module top1(
-
+module top(
+    input [5:0] sw,
+    output led1
     );
+    
+        
+    mux4to1 M0(
+    .x0(sw[0]),
+    .x1(sw[1]),
+    .x2(sw[2]),
+    .x3(sw[3]),
+    .s1(sw[5]),
+    .s0(sw[4]), 
+    .f(led1)
+    
+    );
+    
+    
+    
 endmodule
