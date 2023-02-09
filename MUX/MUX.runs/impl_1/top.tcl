a1d3b935c6dc4269cc34466b762b29774f3030fe
PadLifter <lentodrone@gmail.com>
2023-02-09 11:22:54 +0200

    Project working

--- MUX/MUX.runs/impl_1/top.tcl
+++ MUX/MUX.runs/impl_1/top.tcl
@@ -115,6 +115,8 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Init Design" START { ROLLUP_AUTO }
@@ -122,7 +124,9 @@ start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
+  set_param checkpoint.writeSynthRtdsInDcp 1
   set_param chipscope.maxJobs 1
+  set_param synth.incrementalSynthesisCache C:/Users/35840/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-13748-SBOOKLK/incrSyn
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a35tcpg236-1
   set_property board_part digilentinc.com:basys3:part0:1.2 [current_project]
